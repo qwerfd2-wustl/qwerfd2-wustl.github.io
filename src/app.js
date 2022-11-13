a15b30103fb98097854d3aa1ba3549c848dd0625
qwerfd2 <i5904503668i@gmail.com>
2022-11-12 21:48:04 -0600

    sddsd

--- src/app.js
+++ src/app.js
@@ -1,32 +1,36 @@
+// @flow strict
+
+import type { CommandType } from "./commands.js";
+import type { ClassCommands, JoinOrDiscussType, ClassType } from "./classes.js";
+
 import { COMMANDS } from "./commands.js";
 import { CLASSES } from "./classes.js";
 import { viewHelpPage } from "./help.js";
 
-const redirect = async function (url) {
+const redirect: (string) => Promise<void> = async function (url: string) {
   await window.location.replace(url);
 };
 
-const bunnylol = async function (currCmd) {
-  let arr = [];
-
+const bunnylol: (string) => Promise<boolean> = async function (
+  currCmd: string
+) {
+  let arr: Array<string> = [];
   if (currCmd.startsWith("$")) {
     arr = currCmd.split(/[ $+]/g);
     arr[0] = "$";
-
     if (arr[1] === "") {
       arr = ["$"];
     }
   } else {
     arr = currCmd.split(/[ +]/g);
   }
-
   if (arr.length > 0) {
-    const prefix = arr[0].endsWith(".") ? arr[0].substring(0, arr[0].length - 1).toLowerCase() : arr[0].toLowerCase();
-
+    const prefix: string = arr[0].endsWith(".")
+      ? arr[0].substring(0, arr[0].length - 1).toLowerCase()
+      : arr[0].toLowerCase();
     if (prefix in CLASSES) {
       // $FlowFixMe - this is actually correct since the prefix is a key.
-      const classData = CLASSES[prefix];
-
+      const classData: ClassType = CLASSES[prefix];
       if (arr.length > 1) {
         if (arr[1].toLowerCase() === "j" && classData.zoomurl) {
           await redirect(`${classData.zoomurl}`);
@@ -42,23 +46,23 @@ const bunnylol = async function (currCmd) {
           return true;
         }
       }
-
       await redirect(`${classData.url}`);
       return true;
     }
-
     if (prefix in COMMANDS) {
       // $FlowFixMe - this is actually correct since the prefix is a key.
-      const command = COMMANDS[prefix];
-      const protocol = new URL(command.url).protocol;
-
+      const command: CommandType = COMMANDS[prefix];
+      const protocol: string = new URL(command.url).protocol;
       if (protocol !== "https:" && protocol !== "http:") {
         viewHelpPage();
       }
-
       if (command.searchurl && arr.length !== 1) {
         const searchParam = prefix !== "$" ? prefix.length + 1 : prefix.length;
-        await redirect(`${command.searchurl}${encodeURIComponent(currCmd.substr(searchParam))}`);
+        await redirect(
+          `${command.searchurl}${encodeURIComponent(
+            currCmd.substr(searchParam)
+          )}`
+        );
         return true;
       } else {
         await redirect(command.url);
@@ -66,24 +70,27 @@ const bunnylol = async function (currCmd) {
       }
     }
   }
-
   return false;
 };
 
-const currCmd = new URL(window.location.href).searchParams.get("search") ?? "help";
+const currCmd: string =
+  new URL(window.location.href).searchParams.get("search") ?? "help";
 
 switch (currCmd) {
   case "help" || "":
     viewHelpPage();
     break;
-
   default:
-    bunnylol(currCmd).then(done => {
-      if (!done && COMMANDS.DEFAULT.searchurl) {
-        redirect(`${COMMANDS.DEFAULT.searchurl}${encodeURIComponent(currCmd)}`);
-      }
-    }).catch(reject => {
-      console.log(reject);
-    });
+    bunnylol(currCmd)
+      .then((done: boolean) => {
+        if (!done && COMMANDS.DEFAULT.searchurl) {
+          redirect(
+            `${COMMANDS.DEFAULT.searchurl}${encodeURIComponent(currCmd)}`
+          );
+        }
+      })
+      .catch((reject: string) => {
+        console.log(reject);
+      });
     break;
 }
